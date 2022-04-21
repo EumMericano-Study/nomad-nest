8757d9aadd524f5249c264dcea7c95f4c6fcb56b
Eumericano <opwer032@naver.com>
2022-04-21 23:32:37 +0900

    feat: Test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 데코레이터를 통해 클래스에 함수 기능들을 추가할 수 있다.
 @Module({
   imports: [],
   controllers: [AppController],
