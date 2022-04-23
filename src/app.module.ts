50041d6096e2d50693e0a5a03b7220ae27fe4071
Eumericano <opwer032@naver.com>
2022-04-23 16:00:52 +0900

    feat: Set module injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 /*
  *  데코레이터를 통해 클래스에 함수 기능들을 추가할 수 있다.
  *  데코레이터를 쓸 땐 함수나 클래스 사이에 공백을 두면 안된다.
  */
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
