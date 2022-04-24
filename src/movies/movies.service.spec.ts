f31c1f6e05c1356a2dea981f74dab50a2c71aee7
Eumericano <opwer032@naver.com>
2022-04-24 10:45:17 +0900

    test: Testing getOne function

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -20,6 +21,32 @@ describe('MoviesService', () => {
     it('should be return an array', () => {
       const result = service.getAll();
       expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test movie',
+        year: 2022,
+        genres: ['test'],
+      });
+      const movie = service.getOne(1);
+
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+      expect(movie.title).toEqual('Test movie');
+      expect(movie.year).toEqual(2022);
+      expect(movie.genres).toBeInstanceOf(Array);
+    });
+
+    it('should throw 404 Error', () => {
+      try {
+        service.getOne(9999999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID: 9999999 is not found');
+      }
     });
   });
 });
