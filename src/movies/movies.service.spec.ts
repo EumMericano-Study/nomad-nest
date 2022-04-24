a2c0b9ec8ddb8b6ef0354051baf533b0bfa8e237
Eumericano <opwer032@naver.com>
2022-04-24 11:02:52 +0900

    test: Testing create and delete movies

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -49,4 +49,43 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test delete movie',
+        year: 2022,
+        genres: ['test'],
+      });
+
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should throw 404 Error', () => {
+      try {
+        service.deleteOne(9999999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID: 9999999 is not found');
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test delete movie',
+        year: 2022,
+        genres: ['test'],
+      });
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
 });
