8f99b70ee7400856fbf4fcab2e8f44dc8bb799e9
Eumericano <opwer032@naver.com>
2022-04-26 23:40:43 +0900

    test: Add movie detail CRUD tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,12 +35,25 @@ describe('AppController (e2e)', () => {
     it('GET', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'Test Movies', year: 2022, genres: ['test'] })
         .expect(201);
     });
+
+    it('POST 404', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movies',
+          year: 2022,
+          genres: ['test'],
+          otherThings: true,
+        })
+        .expect(400);
+    });
+
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
@@ -54,8 +67,16 @@ describe('AppController (e2e)', () => {
         return request(app.getHttpServer()).get('/movies/9999999').expect(404);
       });
 
-      it.todo('DELETE');
-      it.todo('PATCH');
+      it('PATCH', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/1')
+          .send({ title: 'hey~' })
+          .expect(200);
+      });
+
+      it('DELETE', () => {
+        return request(app.getHttpServer()).delete('/movies/1').expect(200);
+      });
     });
   });
 });
